34b5f2ebd2829d82d7d12ca0e8e89cc963a993a7
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-06-10 19:58:24 +0530

    2nd commit

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Recipe = () => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return (
         <div>
-            <h1>Title</h1>
-            <p>Calories</p>
-            <img src="" alt="" />
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <img src={image} />
         </div>
     );
 }
